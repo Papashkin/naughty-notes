744ffb38b589eee9fce93ec0f07606783157ab7e
pavelantoshkin <paavoantoshkin@gmail.com>
2025-07-11 12:20:00 +0200

    task: Create Note screen implementation (basis)

--- app/src/main/java/com/antsfamily/sexcalendar/design/navigation/Navigator.kt
+++ app/src/main/java/com/antsfamily/sexcalendar/design/navigation/Navigator.kt
@@ -1,4 +1,4 @@
-package com.antsfamily.sexcalendar.design
+package com.antsfamily.sexcalendar.design.navigation
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.material3.Scaffold
