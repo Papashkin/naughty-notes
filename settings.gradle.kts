ac6d9df14b43790036b473fcec894e15ccdb2c7b
pavelantoshkin <paavoantoshkin@gmail.com>
2025-11-18 13:41:42 +0100

    task: add maven jitpack repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,14 +9,15 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+        maven(url = "https://jitpack.io")
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        maven(url = "https://jitpack.io")
         mavenCentral()
+        maven(url = "https://jitpack.io")
     }
 }
 
