07e40070db7756d345beebb2c5a217f2434ddd95
Pavel Antoshkin <34233903+Papashkin@users.noreply.github.com>
2026-05-08 21:49:20 +0200

    task: Maestro tests in CI/CD (#76)

--- app/src/main/java/com/antsfamily/naughtynotes/presentation/util/ModifierExtensions.kt
+++ app/src/main/java/com/antsfamily/naughtynotes/presentation/util/ModifierExtensions.kt
@@ -70,4 +70,3 @@ fun Modifier.debouncedClickable(
         }
     }
 }
-
---
+++ app/src/main/java/com/antsfamily/naughtynotes/presentation/util/TestTag.kt
@@ -0,0 +1,8 @@
+package com.antsfamily.naughtynotes.presentation.util
+
+enum class TestTag(val value: String) {
+    // Note form
+    NOTE_FORM_SCREEN_PRACTICE_TYPE_DROPDOWN("note_form_screen_practice_type_dropdown"),
+    NOTE_FORM_SCREEN_LOCATION_DROPDOWN("note_form_screen_location_dropdown"),
+    ;
+}
